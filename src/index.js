f334c848746c1eb786a39410170d5393b4912857
Allyson Monteiro <allyson.martins@task-check.com>
2021-06-02 18:14:45 -0300

    eslint configurado

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Text, View} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+// import { Container } from './styles';
+Icon.loadFont();
+const App = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+export default App;
